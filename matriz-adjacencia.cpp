17d0654294f2c5a8ebf5cc421acb2ac7c317e795
Camila <camilacaleones@dcc.ufrj.br>
2023-04-15 09:43:55 -0300

    implementa função de achar vizinhos

--- matriz-adjacencia.cpp
+++ matriz-adjacencia.cpp
@@ -3,29 +3,78 @@
 e vai dizer se aquela aresta existe ou não. O bom de se usar MA é que operações de consultar, remover e adicionar arestas são O(1), mas operações de encontrar vizinhos são O(n) e contar as arestas são O(n²). 
 Outro ponto negativo é que, uma matriz n x n ocupa muita memoria e as operações de inEdge ou outEdge são custosas. 
 */ 
-#include <iostream> 
 
+#include <iostream>
 using namespace std;
 
-class Graph{
-    bool** adjMatrix;
-    int numVertices;
+class Graph {
+   private:
+  bool** adjMatrix;
+  int numVertices;
 
-    Graph(int numVertices){
-        // inicializa o grafo
-        this->numVertices = numVertices;
-        adjMatrix - new bool*[numVertices];
-
-        //agora precisa criar a matriz
-        for(int i = 0; i < numVertices; i++){
-            adjMatrix[i] = new bool[numVertices];
-            for(int j = 0; j  < numVertices; j++){
-                adjMatrix[j] = new bool[numVertices];
-            }
-        }
+   public:
+  // Inicializa a matriz
+  Graph(int numVertices) {
+    this->numVertices = numVertices;
+    adjMatrix = new bool*[numVertices];
+    for (int i = 0; i < numVertices; i++) {
+      adjMatrix[i] = new bool[numVertices];
+      for (int j = 0; j < numVertices; j++)
+        adjMatrix[i][j] = false;
     }
+  }
+
+  // Add arestas
+  void addEdge(int i, int j) {
+    adjMatrix[i][j] = true;
+    adjMatrix[j][i] = true;
+  }
+
+  // Remove arestas
+  void removeEdge(int i, int j) {
+    adjMatrix[i][j] = false;
+    adjMatrix[j][i] = false;
+  }
+
+  // Print matrix
+  void print() {
+    for (int i = 0; i < numVertices; i++) {
+      cout << i << " : ";
+      for (int j = 0; j < numVertices; j++)
+        cout << adjMatrix[i][j] << " ";
+      cout << "\n";
+    }
+  }
+
+  //encontrar todos os vizinhos
+  void findNeighbors(int row){
+    cout << row << " : ";
+    for(int j = row; j <= row; j++){
+        for(int i = 0; i < numVertices; i++){
+            cout << adjMatrix[j][i] << " ";
+        }
+        cout << "\n";
+    }
+  }
+
+  ~Graph() {
+    for (int i = 0; i < numVertices; i++)
+      delete[] adjMatrix[i];
+    delete[] adjMatrix;
+  }
 };
 
-int main(void){
-    printf("criei");
+int main() {
+  Graph g(5);
+
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(2, 3);
+  g.addEdge(3, 1);
+  g.addEdge(3, 3);
+  g.addEdge(3, 1);
+  
+  //printf("aqui");
+  //g.print();
+  g.findNeighbors(2);
 }
